aa80f76ba580cd0b2104e745b089b2258359ce20
Gabriel Schettino <64440555+gabrielschettino@users.noreply.github.com>
2023-01-01 21:13:53 -0300

    Add files via upload

---
+++ src/main/java/br/gabrielschettino/testeIDEA/TesteIdeaApplication.java
@@ -0,0 +1,13 @@
+package br.gabrielschettino.testeIDEA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteIdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteIdeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gabrielschettino/testeIDEA/TesteIdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gabrielschettino.testeIDEA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteIdeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
